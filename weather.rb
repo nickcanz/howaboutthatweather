af7057b9f32edfe80d8cf3e3448bf8924afd6566
Nicholas Canzoneri <ac3522@gmail.com>
2011-07-28 01:02:16 -0400

    Making form to user inpput

--- weather.rb
+++ weather.rb
@@ -6,9 +6,15 @@ require 'haml'
 
 require './noaa_weather'
 
+get '/' do
+  redirect "/#{params['zip']}" unless params['zip'] == nil
+  haml :index
+end
+
 get '/:zip' do
   zip = params['zip'].to_i
   temps = NOAA.current_weather(zip)
+  @zip = zip
   @temps = temps
   @dateformat = "%a  %b %d"
   haml :index
